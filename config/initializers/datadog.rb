efe26ad40277c13e6e73d6d606a8a13af25a96a0
teapuddles <kingsley.gleeson@gmail.com>
2021-09-21 13:01:41 -0400

    datadog

--- config/initializers/datadog.rb
+++ config/initializers/datadog.rb
@@ -1,3 +1,14 @@
 require 'datadog/statsd'
 
 statsd = Datadog::Statsd.new('localhost', 8125)
+
+Datadog.configure do |c|
+    # To enable runtime metrics collection, set `true`. Defaults to `false`
+    # You can also set DD_RUNTIME_METRICS_ENABLED=true to configure this.
+    c.runtime_metrics.enabled = true
+  
+    # Optionally, you can configure the DogStatsD instance used for sending runtime metrics.
+    # DogStatsD is automatically configured with default settings if `dogstatsd-ruby` is available.
+    # You can configure with host and port of Datadog agent; defaults to 'localhost:8125'.
+    c.runtime_metrics statsd: Datadog::Statsd.new
+  end
