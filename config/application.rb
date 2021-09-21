941eeff30b57e6cd4ae98c7261bcc4e6507bbb68
teapuddles <kingsley.gleeson@gmail.com>
2021-09-21 16:01:55 -0400

    moved lograge to development env

--- config/application.rb
+++ config/application.rb
@@ -38,23 +38,5 @@ module Newcovid19tracker
 
     statsd = Datadog::Statsd.new('localhost', 8125)
 
-    # Lograge config
-config.lograge.enabled = true
-
-# This specifies to log in JSON format
-config.lograge.formatter = Lograge::Formatters::Json.new
-
-## Disables log coloration
-config.colorize_logging = false
-
-# Log to a dedicated file
-config.lograge.logger = ActiveSupport::Logger.new(File.join(Rails.root, 'log', "#{Rails.env}.log"))
-
-# This is useful if you want to log query parameters
-  config.lograge.custom_options = lambda do |event|
-      { :ddsource => 'ruby',
-        :params => event.payload[:params].reject { |k| %w(controller action).include? k }
-      }
-    end
   end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -49,4 +49,23 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+      # Lograge config
+config.lograge.enabled = true
+
+# This specifies to log in JSON format
+config.lograge.formatter = Lograge::Formatters::Json.new
+
+## Disables log coloration
+config.colorize_logging = false
+
+# Log to a dedicated file
+config.lograge.logger = ActiveSupport::Logger.new(File.join(Rails.root, 'log', "#{Rails.env}.log"))
+
+# This is useful if you want to log query parameters
+  config.lograge.custom_options = lambda do |event|
+      { :ddsource => 'ruby',
+        :params => event.payload[:params].reject { |k| %w(controller action).include? k }
+      }
+    end
 end
